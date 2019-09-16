10adf930b907fd98a11a12a77077350680d190fa
Tobias Toffee <eclaireexpress@gmail.com>
2019-09-15 22:18:33 -0400

    Initial push

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import FetchSheets from "./components/FetchSheets";
+
+ReactDOM.render(<FetchSheets />, document.getElementById('root'));
